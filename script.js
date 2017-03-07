280bd31c8171e117a78f5040d32e7f873a16694e
meagan <mfeniseycopes@gmail.com>
2017-03-07 17:17:57 -0500

    Extend a component and #diff()

--- script.js
+++ script.js
@@ -3,12 +3,29 @@ import { renderToDOM } from './lib/Utility'
 
 window.Component = Component
 
-renderToDOM(
-  Component, { 
-    html: '<div>Parent</div>',
-    children: [
-      { html: '<div>Child1</div>' },
-      { html: '<div>Child2</div>' }
-    ]
-  }, '#root')
+class ChildComponent extends Component {
+  
+  render() {
+    return {
+      html: `<div>${this.props.parent}</div>`,
+      children: [
+        { html: `<div>${this.props.child1}</div>` },
+        { html: `<div>${this.props.child2}</div>` }
+      ]
+    }
+  }
+}
+
+const firstProps = {
+  parent: 'Papa',
+  child1: 'Nino1',
+  child2: 'Nino2',
+} 
+
+window.testComp = new ChildComponent(firstProps) 
+
+renderToDOM(testComp, '#root')
+
+window.testComp.receiveProps(Object.assign({}, firstProps, { child2: 'Nina2' }))
+window.testComp.diff()
 
