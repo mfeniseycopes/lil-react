7a8f420f180829cae354ae014cf02f4668bd28b6
meagan <mfeniseycopes@gmail.com>
2017-03-20 13:34:52 -0400

    Add object destructuring plugin II

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['es2015'],
+          plugins: [require('babel-plugin-transform-object-rest-spread')]
         }
       }
     ]
