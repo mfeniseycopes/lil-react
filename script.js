f62bda3fc75ac7263a4c1d01ebbf0ac7c6fc68fe
meagan <mfeniseycopes@gmail.com>
2017-03-20 13:35:12 -0400

    Nest children in props

--- script.js
+++ script.js
@@ -2,39 +2,19 @@ import ReactDOM from './lib/ReactDOM'
 import { Component, createElement } from './lib/React'
 import $ from 'jquery'
 
-class Test extends Component {
-  render() {
-    const children = [
-      createElement('<p>P</p>'),
-
-    ]
-
-    if (this.props.p = 'p') {
-      children.push(createElement('<span>SPAN</span>'))
-    }
-
-    return (
-      createElement(
-        `<div>${this.props.div}</div>`,
-        {}, ...children)
-    )
-  }
-}
-
-window.updateComponent = ReactDOM.updateComponent
 window.$ = $
 
 const el1 = createElement(
     'main',
     { span: 'SPAN', div: 'DIV' },
     createElement('hey'),
-    createElement('div'))
+    createElement('hay'))
 
 const el2 = createElement(
     'main',
     { div: 'DIV', p: 'P' },
-    createElement('div'),
-    createElement('div'))
+    createElement('ya'),
+    createElement('yo'))
 
 window.el1 = el1
 
