d679218a2f8570f2d527c39a903d05948f3b0556
meagan <mfeniseycopes@gmail.com>
2017-03-21 16:35:12 -0400

    Remove _old stuffs

--- _old/Element.js
+++
@@ -1,8 +0,0 @@
-
-const createElement = (type, props, ...children) => ({
-  type,
-  props: { ...props, children }, 
-}) 
-
-
-export default createElement 
--- _old/Utility.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery'
-
-export const renderToDOM = (component, selector) => {
-  const parent = { $node: $(selector) }
-  component.mounting(parent)
-  // $(selector).append(component.$node)
-}
